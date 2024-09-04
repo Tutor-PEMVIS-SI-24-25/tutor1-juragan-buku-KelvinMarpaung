47dde391541cdd9e5d04a9db21d3854f25fa1c4e
KelvinMarpaung <yohaneskelvin9@gmail.com>
2024-09-04 21:27:06 +0700

    Telah Menyelesiakan Tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,13 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24018 -KELVIN YOHANES PUTRA
+// 12S24018 -KELVIN YOHANES PUTRA
 
+function main() {
+    var nama;
+
+    nama = window.prompt('Enter a value for nama');
+    window.alert("Nama Saya ");
+    var bilangan;
+
+    bilangan = Number(window.prompt('Enter a value for bilangan'));
+    window.alert("bilangan genap ");
+}
